918c09b2e7d24b761aaa8b00a2a3348ff270c308
thpixel-dev <gamernoob1673@gmail.com>
2024-10-22 17:20:03 -0300

    novo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Body from './componentes/Body'
+import Header from './componentes/Header'
+import App from './App.css'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Body/>
+    </>
+  )
+}
+
+export default App
